ec101fece2f67b72cbbddf0ef515087ac5dba3e9
x <mclelkot.2010@gmail.com>
2017-11-24 16:34:36 +0100

    Blink LED on pin 13 with interval 3000

--- arduino_hello.ino
+++ arduino_hello.ino
@@ -1,9 +1,28 @@
+
+
+
+// HIGH: 1
+// LOW: 0
+
+/*
+ * Sketch uses 1490 bytes (0%) of program storage space. Maximum is 253952 bytes.
+ * Global variables use 9 bytes (0%) of dynamic memory, leaving 8183 bytes for local variables. Maximum is 8192 bytes.
+ */
+
+short WHITE_LED_PIN=13;
+unsigned long long LED_INTERVAL=3000;
+ 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(WHITE_LED_PIN, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(WHITE_LED_PIN, HIGH); // LED on
+  delay(LED_INTERVAL);
+  digitalWrite(WHITE_LED_PIN, LOW);  // LED off
+  delay(LED_INTERVAL);
 
 }
